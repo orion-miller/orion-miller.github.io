3ef92505f97a684e14b3b2ff132f7c1eb89f465c
orion-miller <33074666+orion-miller@users.noreply.github.com>
2023-07-19 17:36:16 -0400

    start image resizer

--- utilities/ImageResizer.py
+++ utilities/ImageResizer.py
@@ -1,11 +1,25 @@
+import os
 from PIL import Image
 
-foo = Image.open('..\\assets\\images\\2023-07-10-adding-bluetooth-to-pontiac-radio\\1.png')  # My image is a 200x374 jpeg that is 102kb large
-foo.size  # (200, 374)
+'''This function will cycle through all the images in the assets/images folder and resize them as needed to ensure fast web page performance. 
+PNG files are converted to JPG because of lower file, and then will be resized to below 500 KB if still larger than that.
+The string "SKIPPNG" can be used in the filename of any PNG image that the JPG conversion needs to be skipped on for any reason'''
+
+FullFileName = os.path.realpath(__file__)
+WorkingDir = FullFileName.split('utilities')[0]
+ImagesDir = WorkingDir + "assets\\images\\"
+
+FileName = ImagesDir+'2023-07-10-adding-bluetooth-to-pontiac-radio\\1.png'
+FileSize = os.path.getsize(FileName)
+
+#Check if file is larger than 1 MB, if so resize it
+if FileSize > 1000000: 
+    IMG = Image.open(FileName)  # My image is a 200x374 jpeg that is 102kb large
+    IMG.size  # (200, 374)
  
 # downsize the image with an ANTIALIAS filter (gives the highest quality)
-foo = foo.resize((160,300),Image.ANTIALIAS)
+IMG = IMG.resize((160,300),Image.ANTIALIAS)
  
-foo.save('path/to/save/image_scaled.jpg', quality=95)  # The saved downsized image size is 24.8kb
+IMG.save('path/to/save/image_scaled.jpg', quality=95)  # The saved downsized image size is 24.8kb
  
-foo.save('path/to/save/image_scaled_opt.jpg', optimize=True, quality=95)  # The saved downsized image size is 22.9kb
+IMG.save('path/to/save/image_scaled_opt.jpg', optimize=True, quality=95)  # The saved downsized image size is 22.9kb
